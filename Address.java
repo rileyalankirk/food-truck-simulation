381d5779f56b010d1ad419ac1844ed6f357a634a
anthonyestephan <estephana02@moravian.edu>
2019-02-27 19:10:56 -0500

    Edited Address to implement comparator, distance() funct, distribution location as static final int. ReadAddress reads from txt file and puts into priority queue and polled out in order of closest to furthest

--- Address.java
+++ Address.java
@@ -1,10 +1,15 @@
+import java.util.Comparator;
 import java.util.Random;
 
-public class Address {
+import static java.lang.Math.sqrt;
+
+public class Address implements Comparable<Address> {
 
     boolean direction; //TRUE, 1 = east, FALSE, 0 = south
     int houseNum; //house numbers are multiples of 10, starting at 100.  However, there are no houses at the multiples of 100s, as the first house on each street would be, for example, 110.
     int streetNum; //street numbers start at 1, and go
+    private static final int DISTRIBUTION_HOUSENUM = 910;
+    private static final int DISTRIBUTION_STREETNUM = 9;
 
     Random rand = new Random();
 
@@ -12,6 +17,16 @@ public class Address {
         direction = rand.nextBoolean();
         houseNum = getRandomHouseNum();
         streetNum = getRandomStreetNum();
+    }
+
+    protected Address(int houseNum, boolean direction, int streetNum){
+     if(houseNum >= 0 && houseNum < 2000)
+        this.houseNum = houseNum;
+
+     this.direction = direction;
+
+     if(streetNum >= 0 && streetNum < 20)
+         this.streetNum = streetNum;
     }
 
     public int getRandomHouseNum(){
@@ -60,9 +75,37 @@ public class Address {
         return streetNum;
     }
 
+    public double distance(){
+        if(!direction)
+            return Math.sqrt(Math.pow(DISTRIBUTION_HOUSENUM - (streetNum * 100), 2) + Math.pow((DISTRIBUTION_STREETNUM * 100) - houseNum, 2));
+
+        return Math.sqrt(Math.pow(DISTRIBUTION_HOUSENUM - houseNum,2) + Math.pow((DISTRIBUTION_STREETNUM * 100) - (streetNum * 100),2));
+    }
 
     @Override
     public String toString() {
         return Integer.toString(getHouseNum()) + " " + directionToString() + " " + Integer.toString(getStreetNum());
+    }
+
+  /*  @Override
+    public int compare(Address o1, Address o2) {
+        if (o1.distance() < o2.distance())
+            return 1;
+
+        if (o1.distance() > o2.distance())
+            return -1;
+
+        return 0;
+    } */
+
+    @Override
+    public int compareTo(Address o) {
+        if (distance() < o.distance())
+            return -1;
+
+        if (distance() > o.distance())
+            return 1;
+
+        return 0;
     }
 }
